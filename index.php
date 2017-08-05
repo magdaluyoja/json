0fa0a3c8b644a27d3c4ed15b5485aef70f388491
magdaluyoja <magdaluyoja@gmail.com>
2017-08-05 13:14:33 +0800

    First Commit - uploading files

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JSON Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="/JSON/styles/styles.css">
+</head>
+<body>
+	<div id="divmenu"><?php include(); ?></div>
+	<h1>JSON Tutorial</h1>
+</body>
+</html>
