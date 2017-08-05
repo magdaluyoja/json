d47f1f3377e2d8330ddbf60fe36ff7416554fbe4
magdaluyoja <magdaluyoja@gmail.com>
2017-08-05 14:47:53 +0800

    3rd push Json PHP Ajax

--- menu.php
+++ menu.php
@@ -2,4 +2,5 @@
   <li><a href="/json" id="home" class="links">Home</a></li>
   <li><a href="/json/internal" id="internal" class="links">Internal</a></li>
   <li><a href="/json/ajax" id="ajax" class="links">Ajax</a></li>
+  <li><a href="/json/php_ajax" id="php_ajax" class="links">PHP Ajax</a></li>
 </ul>
---
+++ php_ajax/php_ajax.js
@@ -0,0 +1,22 @@
+function ajax_get_json(){
+
+	var results = document.getElementById("divresult");
+    var hr = new XMLHttpRequest();
+
+    hr.open("GET", "php_ajax.php", true);
+    hr.setRequestHeader("Content-type", "application/json", true);
+    hr.onreadystatechange = function() {
+	    if(hr.readyState == 4 && hr.status == 200) {
+		    var data = JSON.parse(hr.responseText);
+			results.innerHTML = "";
+			for(var obj in data){
+				results.innerHTML += data[obj].user+" is "+data[obj].age+" and lives in "+data[obj].country+"<hr />";
+			}
+	    }
+    }
+    hr.send(null);
+    results.innerHTML = "requesting...";
+}
+$("document").ready(function(){
+	ajax_get_json();
+});
